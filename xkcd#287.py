288db2e401cf15b178071c9bee5e300cd2355423
ricardoparizotto <ricardo.dparizotto@gmail.com>
2022-04-11 02:30:09 -0300

    xkcd287

--- xkcd#287.py
+++ xkcd#287.py
@@ -1,15 +1,13 @@
 from z3 import *
 
-a, b, c, d, e, f = Ints("a b c d e f")
+#https://sat-smt.codes/SAT_SMT_by_example.pdf
+#https://www.xkcd.com/287/
+
+appetizers = Ints("a b c d e f")
+a, b, c, d, e, f = appetizers
 s = Solver()
 
 s.add(a*2.15 + b*2.75 + c*3.35 + d*3.55 + e*4.20 + f*5.80 == 15.05)
-s.add(a >= 0)
-s.add(b >= 0)
-s.add(c >= 0)
-s.add(d >= 0)
-s.add(e >= 0)
-s.add(f >= 0)
-
+s.add([And(d >= 0) for d in appetizers]) 
 print (s.check())
 print (s.model())
